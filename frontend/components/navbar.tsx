b32e92f76e4d5647e3c760fdcd240d4692144913
Dhruv Gadhavi <dg28gadhavi@gmail.com>
2024-12-02 04:45:20 +0530

    Changed maxWidth from xl to full for making navbar use full width

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -47,7 +47,7 @@ export const Navbar = () => {
   );
 
   return (
-    <NextUINavbar isBordered maxWidth="xl" position="sticky">
+    <NextUINavbar isBordered maxWidth="full" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
